2afbf21eb89b5e0a2ad6e57d8c7c5f0391c23fad
David <davidstepanov@yahoo.com>
2022-10-28 14:01:42 -0400

    Most of the site works

--- backend/phase-3-sinatra-react-project/app/controllers/art_controller.rb
+++ backend/phase-3-sinatra-react-project/app/controllers/art_controller.rb
@@ -6,16 +6,12 @@ class ArtController < Sinatra::Base
         arts = Art.all.to_json
     end
 
-    get '/arts/' do
-        arts = Art.all.to_json
-    end
-
     get '/arts/:id' do
         artsid = Art.find(params[:id]).to_json
     end
 
-    get '/arts/:id/' do
-        artsid = Art.find(params[:id]).to_json
+    get '/arts/:id/user' do
+        artsuser = Art.find(params[:id]).user.to_json
     end
 
     # get '/arts/title/:title' do
@@ -31,16 +27,12 @@ class ArtController < Sinatra::Base
         createarts = Art.create(params).to_json
     end
 
-    post '/arts/' do
-        createarts = Art.create(params).to_json
-    end
-
-    patch '/arts' do
+    patch '/arts/:id' do
         changearts = Art.find(params[:id]).to_json
         changearts.update(title: params[:title], medium: params[:medium], description: params[:description], image: params[:image], size: params[:size], price: params[:price], timestamp: params[:timestamp], tags: params[:tags], user_id: params[:user_id]).to_json
     end
 
-    delete '/arts' do
+    delete '/arts/:id' do
         deletearts = Art.find(params[:id]).destroy
     end
   
--- backend/phase-3-sinatra-react-project/db/schema.rb
+++ backend/phase-3-sinatra-react-project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_27_182454) do
+ActiveRecord::Schema.define(version: 2022_10_28_171117) do
 
   create_table "arts", force: :cascade do |t|
     t.string "title"
